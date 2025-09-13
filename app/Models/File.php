39b0a411ede1679f3321d29b791a6181e1c98be6
Amina Hamoud <amina123@tt.com>
2025-09-14 02:34:04 +0300

    feat: add models and migrations for users, events, tickets, files, notifications

---
+++ app/Models/File.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'file_path',
+        'file_type',
+    ];
+
+    public function event() {
+        return $this->belongsTo(Event::class);
+    }
+}
+
